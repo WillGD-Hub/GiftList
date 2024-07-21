1bbe454142bc0d262a7945e2cd31598ae7e6e11c
WillGD <56427283+WillGD-Hub@users.noreply.github.com>
2024-07-21 18:50:51 +0700

    Update code to adjust alchemy's task requirement

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const MerkleTree = require('../utils/MerkleTree');
+const niceList = require('../utils/niceList.json');
 const verifyProof = require('../utils/verifyProof');
 
 const port = 1225;
@@ -8,14 +10,15 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const merkleTree = new MerkleTree(niceList);
+const MERKLE_ROOT = merkleTree.getRoot();
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
   const body = req.body;
 
   // TODO: prove that a name is in the list 
-  const isInTheList = false;
+  const isInTheList = verifyProof(body.proof, body.name, MERKLE_ROOT);
   if(isInTheList) {
     res.send("You got a toy robot!");
   }
